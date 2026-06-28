cce33899cb6d33bdff72560565c54118e2fa9d4a
bardia-asd <brdyaasdy5@gmail.com>
2026-06-28 21:52:52 +0330

    chore: install lucide-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes/routes";
+import { ShoppingBag } from "lucide-react";
 
 const App = () => {
     return (
@@ -7,6 +8,7 @@ const App = () => {
             <RouterProvider router={router} />
             <h1 class="text-3xl font-bold underline text-text-primary animate-fade-up">
                 Arlowe & Bean
+                <ShoppingBag />
             </h1>
         </main>
     );
